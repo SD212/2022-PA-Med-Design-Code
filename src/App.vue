5067dfc72fddc27a3e7385529426f24d8f56b88d
Sree Dan <sreedansd@gmail.com>
2022-02-12 17:23:44 -0500

    removed helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
     <Problem/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Problem from './components/Problem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     Problem
   }
 }
