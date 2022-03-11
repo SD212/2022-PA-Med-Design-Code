6164ca3df3ff6f9bba6aaadc3b4136d08d3e3826
TiruD <98369067+TiruD@users.noreply.github.com>
2022-03-10 19:58:29 -0500

    added a menu

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
   <div id="app">
- 
-    <Problem/>
+    
+    <Menu/>
+    <Problem v-if="factoring" />
+    <div v-show="trig">
+      Trig Problem
+    </div>
+      
   </div>
 </template>
 
 <script>
 import Problem from './components/Problem.vue'
+import Menu from './components/Menu.vue'
 
 export default {
   name: 'App',
   components: {
-    Problem
+    Problem,
+    Menu,
+  },
+  data() {
+    return {
+      factoring: localStorage.getItem("factoring"),
+      trig: localStorage.getItem("trig")
+    }
+  },
+  mounted() {
+    if (localStorage.factoring) {
+      this.factoring = localStorage.factoring
+    } else if (localStorage.trig) {
+      this.trig = localStorage.trig
+    }
+  },
+  methods: {
+    showProblem() {
+      console.log("showProblem is called")
+      this.factoring = false
+      this.trig = false
+
+      if (localStorage.problemType == "Trig") {
+        this.trig = true
+      } else {
+        this.factoring = true
+      }
+
+    },
+    getFactoring() {
+      this.showProblem()
+      return this.factoring
+    },
+    getTrig() {
+      this.showProblem()
+      return this.trig
+    }
   }
 }
 </script>
